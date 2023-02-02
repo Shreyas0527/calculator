5bd68b464b0ee4cc566fee079ef85fd65a8f7cfb
Shreyas Pawar <116916638+Shreyas0527@users.noreply.github.com>
2023-02-02 16:10:43 +0530

    Add files via upload

---
+++ love calculator/html/index.js
@@ -0,0 +1,11 @@
+prompt("what is your name?");
+prompt("what is their name");
+
+var lovescore = Math.random() * 100 + 1;
+if (lovescore > 70) {
+alert("Yours love score is " + lovescore + "%" + "You love each other");    
+} else {
+    alert("Yours love score is " + lovescore + "%");
+}
+lovescore = Math.floor(lovescore);
+console.log(lovescore);
